406c37570081852544ac95b525e2135c3dbd3edd
scripturesintech <scripturesintech@gmail.com>
2024-04-24 22:54:33 +0530

    check two sum of Int array

---
+++ TwoSumProblem.playground/Contents.swift
@@ -0,0 +1,24 @@
+import Foundation
+import PlaygroundSupport
+
+
+let nums = [2, 7, 11, 15]
+let target = 9
+let result = twoSum(from: nums,
+                    target: target)
+print("Indices of the two numbers that add up to \(target): \(result)")
+
+func twoSum(from input: [Int], target: Int) -> [Int] {
+    var resultDictionary: [Int: Int] = [Int: Int]()
+    
+    for (index, num) in input.enumerated() {
+        print(index, num)
+        var complement = target - num
+        
+        if let complementIndex = resultDictionary[complement] {
+            return [complementIndex, index]
+        }
+        resultDictionary[num] = index
+    }
+    return [0, 0 ]
+}
